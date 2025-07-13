47099b2a5f6185e5ff28cf02702f1917f330076b
NekroMonger25 <nekromonger25@gmail.com>
2025-07-13 21:23:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ async function checkCacheFolder() {
 
 const options = {
     port: process.env.PORT || 3000,
+     host: '0.0.0.0',
     logger: {
         log: (msg) => console.log(`[Stremio] ${msg}`),
         error: (msg) => console.error(`[Stremio] ${msg}`)
